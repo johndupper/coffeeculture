7073349915806f78a2140a62d742936eb0245ea3
John Dupper <john.dupper@me.com>
2016-12-14 13:13:09 -0500

    changed post to have .user .title .content .website

--- models/post.js
+++ models/post.js
@@ -3,9 +3,10 @@ var mongoose = require('mongoose');
 var PostSchema = new mongoose.Schema({
         user:          { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
         title:         { type: String,  required: true },
-        completed:     { type: Boolean, required: true }
+        content:       { type: String, required: true },
+        website:       { type: String }
     },
-    { timestamps: true }  // createdAt, updatedAt
+    { timestamps: true }
 );
 
 function date2String(date) {
