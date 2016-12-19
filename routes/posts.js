06168ce751a030aa8dc16abb44dc683745cf10d8
John Dupper <john.dupper@me.com>
2016-12-18 19:30:37 -0500

    removed comment

--- routes/posts.js
+++ routes/posts.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 var Post = require('../models/post');
-var Comment = require('../models/comment');
 
 function makeError(res, message, status) {
     res.statusCode = status;
