997d221728eee266598ffdc282d977e1691b12c4
John Dupper <john.dupper@me.com>
2016-12-19 19:54:51 -0500

    changed property to text from comment

--- models/comment.js
+++ models/comment.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var CommentSchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     post: { type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true },
-    comment: { type: String, required: true }
+    text: { type: String, required: true }
 }, {
     timestamps: true
 });
